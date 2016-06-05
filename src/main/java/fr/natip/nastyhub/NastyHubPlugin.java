10ef3635aa58b4f749d689fc4faefeceff70f0fe
NatipCube <natipcubemc@gmail.com>
2016-06-05 22:46:45 +0200

    Update NastyHubPlugin.java

--- src/main/java/fr/natip/nastyhub/NastyHubPlugin.java
+++ src/main/java/fr/natip/nastyhub/NastyHubPlugin.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import fr.natip.nastyhub.listeners.SpawnListener;
 
-
 public class NastyHubPlugin extends JavaPlugin {
 	public void onEnable() {
         getConfig().options().copyDefaults(true);
@@ -14,7 +13,5 @@ public class NastyHubPlugin extends JavaPlugin {
 		Bukkit.getPluginManager().registerEvents(new DamageListener(this), this);
 		Bukkit.getPluginManager().registerEvents(new JoinListener(this), this);
 		Bukkit.getPluginManager().registerEvents(new SpawnListener(this), this);
-		
 	}
-	
 }
