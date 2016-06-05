606d265e13498512c3ca8d9fb926336c09411779
NatipCube <natipcubemc@gmail.com>
2016-06-05 21:57:00 +0200

    Initial commit

--- src/main/java/fr/natip/nastyhub/listeners/DamageListener.java
+++ src/main/java/fr/natip/nastyhub/listeners/DamageListener.java
@@ -1,1 +1,50 @@
+package fr.natip.nastyhub.listeners;
 
+import fr.natip.nastyhub.NastyHubPlugin;
+import org.bukkit.GameMode;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class DamageListener implements Listener {
+	
+    NastyHubPlugin plugin;
+
+    public DamageListener(NastyHubPlugin instance) {
+        this.plugin = instance;
+    }
+	
+	@EventHandler
+	public void onDamage(EntityDamageEvent e) {
+		if (e.getEntityType() != EntityType.PLAYER) {
+			return;
+		}
+	
+		
+			Player p = (Player) e.getEntity();
+			if (!plugin.getConfig().getList("healthWorlds").contains(p.getWorld().getName())) {
+				return;
+			}
+			
+			e.setCancelled(true);		
+	    }
+	
+	@EventHandler
+	public void onFood(FoodLevelChangeEvent e) {
+		if (e.getEntityType() != EntityType.PLAYER) {
+			return;
+		}
+	
+		
+			Player p = (Player) e.getEntity();
+			if (!plugin.getConfig().getList("foodWorlds").contains(p.getWorld().getName())) {
+				return;
+			}
+			
+			e.setCancelled(true);		
+	}
+}
