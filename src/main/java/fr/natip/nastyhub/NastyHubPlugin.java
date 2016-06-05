d968613bbb88dc21379dfb54eb825490169f2497
Kevin Jimenez <keke1422@gmail.com>
2016-06-05 23:52:10 +0200

    Add WeatherListener

--- src/main/java/fr/natip/nastyhub/NastyHubPlugin.java
+++ src/main/java/fr/natip/nastyhub/NastyHubPlugin.java
@@ -2,16 +2,18 @@ package fr.natip.nastyhub;
 
 import fr.natip.nastyhub.listeners.DamageListener;
 import fr.natip.nastyhub.listeners.JoinListener;
+import fr.natip.nastyhub.listeners.WeatherListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import fr.natip.nastyhub.listeners.SpawnListener;
 
 public class NastyHubPlugin extends JavaPlugin {
-	public void onEnable() {
+public void onEnable() {
         getConfig().options().copyDefaults(true);
         saveConfig();
         Bukkit.getPluginManager().registerEvents(new DamageListener(this), this);
-	Bukkit.getPluginManager().registerEvents(new JoinListener(this), this);
-	Bukkit.getPluginManager().registerEvents(new SpawnListener(this), this);
-	}
+        Bukkit.getPluginManager().registerEvents(new JoinListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new SpawnListener(this), this);
+	Bukkit.getPluginManager().registerEvents(new WeatherListener(this), this);
+     }
 }
